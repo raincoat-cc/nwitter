c81c662a72b0bc3c47b9341e725c79f6dffd913e
WooSangWook <ww57403@naver.com>
2022-02-09 15:22:47 +0900

    3.1 Nweeting!

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
+import { dbService } from "fbase";
+import { addDoc, collection } from "firebase/firestore";
 const Home = () => {
   const [nweet, setNweet] = useState("");
   const onSubmit = async event => {
     event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "nweets"), {
+        nweet,
+        createdAt: Date.now(),
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+    setNweet("");
   };
   const onChange = event => {
     const {
