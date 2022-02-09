f5fdfb9c15b54819d43eb6a64ee5534a0a9a4603
WooSangWook <ww57403@naver.com>
2022-02-09 15:20:41 +0900

    3.0 Form and Database Setup

--- src/fbase.js
+++ src/fbase.js
@@ -1,9 +1,11 @@
 import * as firebase from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
@@ -11,3 +13,4 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 export const authService = getAuth();
+export const dbService = getFirestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,28 @@
-import React from "react";
-const Home = () => <span>Home</span>;
+import React, { useState } from "react";
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = async event => {
+    event.preventDefault();
+  };
+  const onChange = event => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="what's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </>
+  );
+};
 export default Home;
