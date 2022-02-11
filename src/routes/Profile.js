8e43100d8d913da7817c392ffe0ba593fba64846
WooSangWook <ww57403@naver.com>
2022-02-11 11:36:51 +0900

    5.0 Get My Own Nweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,10 +1,33 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, dbService } from "fbase";
+import {
+  collection,
+  getDocs,
+  query,
+  QuerySnapshot,
+  where,
+} from "firebase/firestore";
+import React, { useEffect } from "react";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const onLogOutClick = () => {
     authService.signOut();
   };
+
+  const getMyNweets = async () => {
+    const q = query(
+      collection(dbService, "nweets"),
+      where("creatorId", "==", userObj.uid)
+    );
+
+    const QuerySnapshot = await getDocs(q);
+    QuerySnapshot.forEach(doc => {
+      console.log(doc.id, "=>", doc.data());
+    });
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
