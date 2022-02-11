3ab70dc75f009b3c885d1278b226fb792f1ede40
WooSangWook <ww57403@naver.com>
2022-02-11 13:39:10 +0900

    5.1 Update Profile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navigation = () => {
+const Navigation = ({ userObj }) => {
   return (
     <ul>
       <li>
         <Link to="/home">Home</Link>
       </li>
       <li>
-        <Link to="/profile">profile</Link>
+        <Link to="/profile">{userObj.displayName} 의 profile</Link>
       </li>
     </ul>
   );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,35 +1,36 @@
-import { authService, dbService } from "fbase";
-import {
-  collection,
-  getDocs,
-  query,
-  QuerySnapshot,
-  where,
-} from "firebase/firestore";
-import React, { useEffect } from "react";
+import { authService } from "fbase";
+import { updateProfile } from "@firebase/auth";
+import React, { useState } from "react";
 
 const Profile = ({ userObj }) => {
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
   };
-
-  const getMyNweets = async () => {
-    const q = query(
-      collection(dbService, "nweets"),
-      where("creatorId", "==", userObj.uid)
-    );
-
-    const QuerySnapshot = await getDocs(q);
-    QuerySnapshot.forEach(doc => {
-      console.log(doc.id, "=>", doc.data());
-    });
+  const onChange = event => {
+    const {
+      target: { value },
+    } = event;
+    console.log(value);
+    setNewDisplayName(value);
   };
-
-  useEffect(() => {
-    getMyNweets();
-  }, []);
+  const onSubmit = async event => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await updateProfile(userObj, { displayName: newDisplayName });
+    }
+  };
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Display Name"
+          defaultValue={newDisplayName}
+          onChange={onChange}
+        />
+        <input type="submit" value={"Update Profile"} />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
